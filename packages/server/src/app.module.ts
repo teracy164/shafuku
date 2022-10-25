e0ce9d6080f2f1e18903485038728125c3238f74
teracy164 <literacy1945@gmail.com>
2022-10-26 07:17:15 +0900

    static serve

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { DatabaseModule } from './database.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ApiModule, DatabaseModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'front/dist'),
+    }),
+    ApiModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
--- packages/server/src/database.module.ts
+++ packages/server/src/database.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { SeederModule } from 'nestjs-sequelize-seeder';
+import { Dialect } from 'sequelize';
 
 @Module({
   imports: [
     ApiModule,
     SequelizeModule.forRoot({
-      dialect: 'mysql',
+      dialect: (process.env.DB_DIALECT as Dialect) || 'mysql',
       host: process.env.DB_HOST || 'localhost',
       port: Number(process.env.DB_PORT || 3306),
       username: process.env.DB_USERNAME || 'root',
